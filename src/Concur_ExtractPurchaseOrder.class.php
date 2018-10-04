9c772dbfd435b4cd7a6b08fc6c894db27689269c
Paul Gomez <pauldro952@outlook.com>
2018-10-04 12:00:59 -0500

    Initial commit

---
+++ src/Concur_ExtractPurchaseOrder.class.php
@@ -0,0 +1,22 @@
+<?php 
+	/**
+	 * Class to handle dealing with List Items
+	 */
+	namespace dplus\sapconcur;
+	
+	class Concur_ExtractPurchaseOrder extends Concur_Extract {
+        /**
+         * Extract Endpoint
+         * @var string
+         */
+        protected $extract = 'purchase-order';
+        
+        
+        public function import_concurpurchaseorders() {
+            $this->get_extractinfo();
+            $this->get_extractjobs();
+            $this->get_jobstatus();
+            $this->get_extractfile();
+            $this->write_extractfile();
+        }
+    }
